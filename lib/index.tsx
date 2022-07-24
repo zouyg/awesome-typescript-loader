0892576f29b581ccb28b946d191b1f8ecec60cf1
zouyg <zouyiguo@outlook.com>
2022-07-24 12:29:24 +0800

    创建 icon 目录

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { createRoot } from 'react-dom/client';
-import Icon from './icon';
+import Icon from './icon/icon';
 import '../icons/wechat.svg'
 import '../icons/taobao.svg'
 
